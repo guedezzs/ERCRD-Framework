76a72d7c4f642d78589853a47bd06d1605b7249f
guedezzs <guedesrj@msn.com>
2025-11-23 14:45:46 -0300

    Create init.py

---
+++ src/ercrd/init.py
@@ -0,0 +1,20 @@
+"""
+ERCRD Framework: Mathematical Foundation for Dynamic Resource Optimization
+Author: Fabiano Mello Guedes
+Contact: professor@fabianoguedes.com.br
+Repository: https://github.com/fabianomelloguedes/ERCRD-Framework
+
+This software implements the mathematical framework described in:
+"ERCRD: A Unified Mathematical Approach to Dynamic Resource Optimization"
+
+For research and academic use only.
+Commercial use requires authorization.
+"""
+
+from .core import ERCRDOptimizer, EnergyERCRD
+
+__version__ = "1.0.0"
+__author__ = "Fabiano Mello Guedes"
+__email__ = "professor@fabianoguedes.com.br"
+
+__all__ = ['ERCRDOptimizer', 'EnergyERCRD']
